6b97d29736311d09099c89a92da7baf66f77be53
Wesley Mota <wesleymotabr@gmail.com>
2023-06-23 20:46:53 -0300

    Fix return values and update interface to new values

--- interface.py
+++ interface.py
@@ -30,6 +30,9 @@ class Content:
 FnNotify: TypeAlias = Callable[[list[Tuple[Topic, Content]]], None]
 
 class BrokerService(rpyc.Service): # type: ignore
+    def __init__(self):
+        self.topics = {}
+        self.subscribers = {}
 
     # Não é exposed porque só o "admin" tem acesso
     def create_topic(self, id: UserId, topicname: str) -> Topic:
@@ -40,81 +43,56 @@ class BrokerService(rpyc.Service): # type: ignore
 
     # Handshake
 
-    def exposed_login(self, username: str) -> Optional[UserId]:
+    def exposed_login(self, username: str)-> bool:
         # Verificar se o usuário já está logado
         for user_id, user_info in self.users.items():
             if user_info.user_name == username:
-                return user_id
+                return False
         # Gerar um novo ID de usuário
         new_user_id = max(self.users.keys(), default=0) + 1
         self.users[new_user_id] = UserInfo(new_user_id, username)
-        return new_user_id
-
-    # Query operations
-
-    def exposed_get_user_info(self, id: UserId) -> UserInfo:
-        if id not in self.users:
-            raise ValueError(f"The user '{id}' doesn't exist.")
-        return self.users.get(id)
+        return True
 
     def exposed_list_topics(self) -> list[Topic]:
         return list(self.topics.keys())
 
     # Publisher operations
 
-    def exposed_publish(self, id: UserId, topic: Topic, data: str) -> None:
+    def exposed_publish(self, id: UserId, topic: Topic, data: str) -> bool:
         # Verificar se o tópico existe
         if topic not in self.topics:
-            raise ValueError(f"The topic '{topic}' doesn't exist.")
+            return False
         # Criar o conteúdo do anúncio
         content = Content(id, topic, data)
         # Notificar todos os inscritos no tópico
         subscribers = self.topics[topic]
         for subscriber_id, callback in subscribers:
             callback([(topic, content)])
+        return True
 
     # Subscriber operations
 
-    def exposed_subscribe_to(self, id: UserId, topic: Topic, callback: FnNotify) -> Optional[FnNotify]:
+    def exposed_subscribe_to(self, id: UserId, topic: Topic) -> bool:
         # Verificar se o tópico existe
         if topic not in self.topics:
-            raise ValueError(f"The topic '{topic}' doesn't exist.")
+            return False
         # Verificar se o usuário já está inscrito no tópico
         subscribers = self.topics[topic]
         for subscriber_id, _ in subscribers:
             if subscriber_id == id:
-                return "You are already subscribed to this topic."
+                return False
         # Adicionar o usuário aos inscritos
         self.topics[topic].append((id, callback))
-        return callback
+        return True
 
-    def exposed_unsubscribe_to(self, id: UserId, topic: Topic) -> Optional[FnNotify]:
+    def exposed_unsubscribe_to(self, id: UserId, topic: Topic) -> bool:
         # Verificar se o tópico existe
         if topic not in self.topics:
-            raise ValueError(f"The topic '{topic}' doesn't exist.")
+            return False
         # Remover o usuário dos inscritos
         subscribers = self.topics[topic]
         for i, (subscriber_id, callback) in enumerate(subscribers):
             if subscriber_id == id:
-                return subscribers.pop(i)[1]
-        return "You are not subscribed to this topic."
-
-    # Nota: A semântica do subscribe_all não foi definida, opções:
-    # 0 - Jogar essa função fora (e unsubscribe_all)
-    # 1 - Se um topico for criado depois, deve inscrever o User nele
-    # 2 - Se um topico for criado depois, não deve inscrever o User nele
-    #
-    # a: nos casos (0) e (2) fazem sentido se inscrever no "evento" de "novo tópico"
-    def exposed_subscribe_all(self, id: UserId, callback: FnNotify) -> Optional[FnNotify]:
-        # Inscrever o usuário em todos os tópicos existentes
-        for topic in self.topics:
-            self.exposed_subscribe_to(id, topic, callback)
-        return callback
-
-    # Nota: não sei como deve ser o retorno dessa função,
-    # talvez: dict[Topic, Optional[FnNotify]]
-    def exposed_unsubscribe_all(self, id: UserId) -> FnNotify:
-        # Cancelar a inscrição do usuário em todos os tópicos existentes\
-        for topic in self.topics:
-            self.exposed_unsubscribe_to(id, topic)
-        return 'You are not subscribed to any topic anymore.'
+                subscribers.pop(i)[1]
+                return True
+        return True
